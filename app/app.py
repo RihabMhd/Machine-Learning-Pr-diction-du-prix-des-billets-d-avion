ce72608ead4e7a635df1320a1db6082578aa1df2
RihabMhd <rihabmahdi19@gmail.com>
2026-09-25 15:33:10 +0000

    `fix: track model artifact (flight_price_model.joblib) in repo`

--- app/app.py
+++ app/app.py
@@ -3,7 +3,12 @@ import pandas as pd
 import joblib
 from pathlib import Path
 
+from huggingface_hub import hf_hub_download
 
+MODEL_PATH = hf_hub_download(
+    repo_id="rihabmhd/flight-price-model",
+    filename="flight_price_model.joblib"
+)
 
 #configuration
 
